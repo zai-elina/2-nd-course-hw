72a74ce2f41f3ccbbc95497e1ab1794b9849687b
Элина <@COMPUTER>
2023-02-16 22:28:59 +0300

    Add js hw-2.js, mobile version

---
+++ hw-2.js
@@ -0,0 +1,64 @@
+//Задание 1
+// let a=10;
+// alert(a);
+// a=20;
+// alert(a);
+
+//Задание 2
+// const yearOfTheFistIphone=2007;
+// alert(yearOfTheFistIphone);
+
+//Задание 3
+// const jsCreator = 'Брендан Эйх';
+// alert(jsCreator);
+
+//Задание 4
+// let val1 = 10;
+// let val2 = 2;
+// alert(`Сумма: ${val1+val2}, разность:  ${val1-val2}, произведение:  ${val1*val2}, частное:  ${val1/val2}`);
+
+//Задание 5
+// let result = 2**5;
+// alert(result);
+
+//Задание 6
+// let a=9;
+// let b=2;
+// alert(a%b);
+
+//Задание 7
+// let num = 1;
+// num += 5;
+// num -= 3;
+// num *= 7;
+// num /= 3;
+// num++;
+// num--;
+// alert(num);
+
+//Задание 8
+// let age=+prompt("Сколько вам лет?")
+// alert(age)
+
+
+//Задание 9.0
+/*const user={
+    name:'Света',
+    age:30,
+    isAdmin:true,
+}
+//Задание 9.1
+user['city of residence']='Moscow';
+//Задание 9.2
+user.age=18;
+//Задание 9.3
+delete user['city of residence'];
+//Задание 9.4
+let info=prompt("Какую информацию хотите узнать о пользователе?",'name')
+alert(user[info])*/
+
+
+
+//Задание 10
+// let userName = prompt('Как вас зовут?');
+// alert(`Привет, ${userName}!`)
