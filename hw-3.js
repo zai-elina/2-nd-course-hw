36f2a3aaff187ad81fafb92ffed677e12582cde7
Элина <@COMPUTER>
2023-02-20 21:25:12 +0300

    Homework 3

---
+++ hw-3.js
@@ -0,0 +1,114 @@
+//Задание 1
+// let password = 'пароль';
+// let importPassword = prompt('Введите пароль');
+
+// if (password === importPassword) {
+//     alert("Пароль введен верно");
+// } else {
+//     alert("Пароль введен неправильно");
+// }
+
+//Задание 2
+// let c=2;
+// if (c>0 && c<10) {
+//     console.log("Верно");
+// } else {
+//     console.log("Неверно");
+// }
+
+//Задание 3
+// let d = 458;
+// let e = 43;
+// if (d > 100 || e > 100) {
+//   console.log("Верно");
+// } else {
+//   console.log("Неверно");
+// }
+
+//Задание 4
+// let a = '2';
+// let b = '3';
+// alert(+a + +b);
+
+//Задание 5
+// let monthNumber = 10;
+
+// switch (monthNumber) {
+//     case 1:
+//         console.log('зима');
+//         break;
+//     case 2:
+//         console.log('зима');
+//         break;
+//     case 12:
+//         console.log('зима');
+//         break;
+//     case 3:
+//         console.log('весна');
+//         break;
+//     case 4:
+//         console.log('весна');
+//         break;
+//     case 5:
+//         console.log('весна');
+//         break;
+//     case 6:
+//         console.log('лето');
+//         break;
+//     case 7:
+//         console.log('лето');
+//         break;
+//     case 8:
+//         console.log('лето');
+//         break;
+//     case 9:
+//         console.log('осень');
+//         break;
+//     case 10:
+//         console.log('осень');
+//         break;
+//     case 11:
+//         console.log('осень');
+//         break;
+//     default:
+//         console.log('Не существует такого номера месяца');
+//         break;
+// }
+
+//Задание 7
+// let number = +prompt("Пожалуйста, введите любое число");
+
+// if (isNaN(number)) {
+//   alert("Вы неправильно ввели число");
+// } else if (number % 2 === 0) {
+//   alert("Число четное");
+// } else {
+//   alert("Число нечетное");
+// }
+
+//Задание 8
+// let clientOS = 0;
+
+// if (clientOS === 0){
+//     console.log('Установите версию приложения для iOS по ссылке');
+// }else{
+//     console.log('Установите версию приложения для Android по ссылке');
+// }
+
+//Задание 9
+let clientOS = 1;
+let clientDeviceYear = 2015;
+
+if (clientDeviceYear >= 2015) {
+  if (clientOS === 0) {
+    console.log("Установите версию приложения для iOS по ссылке");
+  } else {
+    console.log("Установите версию приложения для Android по ссылке");
+  }
+} else {
+  if (clientOS === 0) {
+    console.log("Установите облегченную версию приложения для iOS по ссылке");
+  } else {
+    console.log("Установите облегченную версию приложения для iOS по ссылке");
+  }
+}
